1eaa5f52dc178a1bec9c8d8fb56f109eb6ea3c68
Alexander Myltsev <alexander@myltsev.com>
2016-06-01 16:46:43 +0300

    = Merge JsonModel files

---
+++ front/app/models/JsonModel.scala
@@ -0,0 +1,18 @@
+package models
+
+import play.api.libs.json._
+import java.util.UUID
+
+case class Name(id: UUID, value: String)
+case class NameString(name: Name, canonicalName: Name)
+case class DataSource(title: String, description: String)
+case class Match(nameString: NameString)
+case class Response(total: Int, matches: Seq[Match])
+
+object Formatters {
+  implicit val nameFormat       = Json.format[Name]
+  implicit val nameStringFormat = Json.format[NameString]
+  implicit val dataSourceFormat = Json.format[DataSource]
+  implicit val matchFormat      = Json.format[Match]
+  implicit val responseFormat   = Json.format[Response]
+}
--- front/app/models/NameString.scala
+++
@@ -1,8 +0,0 @@
-package models
-
-import java.util.UUID
-
-case class Name(id: UUID, value: String)
-case class NameString(name: Name, canonicalName: Name)
-case class Match(nameString: NameString)
-case class Response(total: Int, matches: Seq[Match])
