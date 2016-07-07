613a73a516492efd1c237e6eaf3f4ddb722d0134
Alexander Myltsev <alexander@myltsev.com>
2016-07-07 13:55:08 +0300

    = Fix docker startup script

--- bin/docker-startup.sh
+++ bin/docker-startup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while [[ "$(pg_isready -h $DB_HOST -U $DB_USER)" =~ "no response" ]]; do
+while [[ "$(pg_isready -h ${DB_HOST} -U ${DB_USER})" =~ "no response" ]]; do
   echo "Waiting for postgres to start..."
   sleep 0.1
 done
@@ -8,11 +8,11 @@ done
 declare -a databases=("development" "test_api" "test_resolver")
 db_dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )/../db-migration" && pwd )"
 
-cd $db_dir
+cd ${db_dir}
 
 for db in "${databases[@]}"
 do
-  psql -h $DB_HOST -U $DB_USER -tc \
+  psql -h ${DB_HOST} -U ${DB_USER} -tc \
     "SELECT 1 FROM pg_database WHERE datname = '$db'" \
     | grep -q 1 || psql -h $DB_HOST -U $DB_USER -c "CREATE DATABASE $db"
 
