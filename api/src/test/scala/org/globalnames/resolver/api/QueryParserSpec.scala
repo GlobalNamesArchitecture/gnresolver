bf660c66727ba1145a235e0f5121fda8c6f3a2ed
Dmitry Mozzherin <dmozzherin@gmail.com>
2016-10-04 11:16:19 -0500

    = add DataSourceSpec

--- api/src/test/scala/org/globalnames/resolver/api/QueryParserSpec.scala
+++ api/src/test/scala/org/globalnames/resolver/api/QueryParserSpec.scala
@@ -1,6 +1,9 @@
-package org.globalnames.resolver.api
+package org.globalnames
+package resolver
+package api
 
 import org.scalatest.FunSpec
+import org.scalatest.Matchers._
 import org.globalnames.resolver.api.QueryParser.{Modifier, SearchPart}
 
 class QueryParserSpec extends FunSpec {
@@ -9,109 +12,109 @@ class QueryParserSpec extends FunSpec {
   describe(".result") {
     context("modifiers") {
       it("works without modifier") {
-        assert(QueryParser.result("Pomatomus") ==
-          SearchPart(Modifier("none"), "Pomatomus", false))
+        QueryParser.result("Pomatomus") shouldBe
+          SearchPart(Modifier("none"), "Pomatomus", false)
       }
 
       it("recognises 'exact' modifier") {
-        assert(QueryParser.result("exact:Pomatomus") ==
-          SearchPart(Modifier("exact"), "Pomatomus", false))
+        QueryParser.result("exact:Pomatomus") shouldBe
+          SearchPart(Modifier("exact"), "Pomatomus", false)
       }
 
       it("recognises 'name string' modifier"){
-        assert(QueryParser.result("ns:Pomatomus") ==
-          SearchPart(Modifier("ns"), "Pomatomus", false))
+        QueryParser.result("ns:Pomatomus") shouldBe
+          SearchPart(Modifier("ns"), "Pomatomus", false)
       }
 
       it("recognises 'canonical form' modifier") {
-        assert(QueryParser.result("can:Pomatomus") ==
-          SearchPart(Modifier("can"), "Pomatomus", false))
+        QueryParser.result("can:Pomatomus") shouldBe
+          SearchPart(Modifier("can"), "Pomatomus", false)
       }
 
       it("recognises 'uninomial' modifier") {
-        assert(QueryParser.result("uni:Pomatomus") ==
-          SearchPart(Modifier("uni"), "Pomatomus", false))
+        QueryParser.result("uni:Pomatomus") shouldBe
+          SearchPart(Modifier("uni"), "Pomatomus", false)
       }
 
       it("recognises 'genus' modifier") {
-        assert(QueryParser.result("gen:Pomatomus") ==
-          SearchPart(Modifier("gen"), "Pomatomus", false))
+        QueryParser.result("gen:Pomatomus") shouldBe
+          SearchPart(Modifier("gen"), "Pomatomus", false)
       }
 
       it("recognises 'species' modifier") {
-        assert(QueryParser.result("sp:saltator") ==
-          SearchPart(Modifier("sp"), "saltator", false))
+        QueryParser.result("sp:saltator") shouldBe
+          SearchPart(Modifier("sp"), "saltator", false)
       }
 
       it("recognises 'infraspecies' modifier") {
-        assert(QueryParser.result("ssp:sapiens") ==
-          SearchPart(Modifier("ssp"), "sapiens", false))
+        QueryParser.result("ssp:sapiens") shouldBe
+          SearchPart(Modifier("ssp"), "sapiens", false)
       }
 
       it("recognises 'author' modifier") {
-        assert(QueryParser.result("au:Linn") ==
-          SearchPart(Modifier("au"), "Linn", false))
+        QueryParser.result("au:Linn") shouldBe
+          SearchPart(Modifier("au"), "Linn", false)
       }
 
       it("recognises 'year' modifier") {
-        assert(QueryParser.result("yr:1889") ==
-          SearchPart(Modifier("yr"), "1889", false))
+        QueryParser.result("yr:1889") shouldBe
+          SearchPart(Modifier("yr"), "1889", false)
       }
     }
 
     context("wild card") {
       it("recognises wild card without modifiers") {
-        assert(QueryParser.result("Pom*") ==
-          SearchPart(Modifier("none"), "Pom", true))
+        QueryParser.result("Pom*") shouldBe
+          SearchPart(Modifier("none"), "Pom", true)
       }
 
       it("recognises wild card with modifiers") {
-        assert(QueryParser.result("gen:Pom*") ==
-          SearchPart(Modifier("gen"), "Pom", true))
+        QueryParser.result("gen:Pom*") shouldBe
+          SearchPart(Modifier("gen"), "Pom", true)
       }
     }
 
     context("edge cases") {
       it("ignores unknown modifiers") {
-        assert(QueryParser.result("what:Something") ==
-          SearchPart(Modifier("none"), "what:Something", false))
+        QueryParser.result("what:Something") shouldBe
+          SearchPart(Modifier("none"), "what:Something", false)
       }
 
       it("ignores missing modifier") {
-        assert(QueryParser.result(":Something") ==
-          SearchPart(Modifier("none"), ":Something", false))
+        QueryParser.result(":Something") shouldBe
+          SearchPart(Modifier("none"), ":Something", false)
       }
 
       it("ignores missplaced modifier") {
-        assert(QueryParser.result("Something:ssp") ==
-          SearchPart(Modifier("none"), "Something:ssp", false))
+        QueryParser.result("Something:ssp") shouldBe
+          SearchPart(Modifier("none"), "Something:ssp", false)
       }
 
       it("ignores chained modifiers") {
-        assert(QueryParser.result("yr:sp:1887") ==
-          SearchPart(Modifier("none"), "yr:sp:1887", false))
+        QueryParser.result("yr:sp:1887") shouldBe
+          SearchPart(Modifier("none"), "yr:sp:1887", false)
       }
 
       it("recognises wildcard ignoring bad modifiers") {
-        assert(QueryParser.result("yr:sp:1887*") ==
-          SearchPart(Modifier("none"), "yr:sp:1887", true))
+        QueryParser.result("yr:sp:1887*") shouldBe
+          SearchPart(Modifier("none"), "yr:sp:1887", true)
       }
     }
 
     context("BUGS") {
       it("is confused by text starting as the same modifier") {
-        assert(QueryParser.result("sp:spathulata") ==
-          SearchPart(Modifier("none"), "sp:spathulata", false))
+        QueryParser.result("sp:spathulata") shouldBe
+          SearchPart(Modifier("none"), "sp:spathulata", false)
       }
 
       it("is confused by text starting as a different modifier") {
-        assert(QueryParser.result("au:spathulata") ==
-          SearchPart(Modifier("none"), "au:spathulata", false))
+        QueryParser.result("au:spathulata") shouldBe
+          SearchPart(Modifier("none"), "au:spathulata", false)
       }
 
       it("ignores modifier repeated without text") {
-        assert(QueryParser.result("yr:yr") ==
-          SearchPart(Modifier("none"), "yr:yr", false))
+        QueryParser.result("yr:yr") shouldBe
+          SearchPart(Modifier("none"), "yr:yr", false)
       }
     }
   }
