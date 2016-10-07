f52acd6682ca13ae65034d5a3d77bf1336be5b62
Alexander Myltsev <alexander@myltsev.com>
2016-10-07 12:52:25 +0300

    = Add CrossMapIntegrationSpec

--- resolver/src/main/scala/org/globalnames/resolver/CrossMap.scala
+++ resolver/src/main/scala/org/globalnames/resolver/CrossMap.scala
@@ -6,8 +6,6 @@ import slick.driver.PostgresDriver.api._
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
-import scalaz._
-import Scalaz._
 
 class CrossMap(db: Database) {
   val nameStringIndicies = TableQuery[NameStringIndices]
@@ -15,26 +13,30 @@ class CrossMap(db: Database) {
 
   def execute(databaseSourceId: Int, databaseTargetId: Int,
               localIds: Seq[String]): Future[Seq[(String, String)]] = {
-    // Slick doesn't give facilities to create temporary table from `localIds` and
-    // make join against it. Until https://github.com/slick/slick/issues/799 is solved
-    val existingCrossMapsQuery = crossMaps.filter { cm =>
-      cm.dataSourceId === databaseSourceId && cm.localId.inSetBind(localIds)
+    if (databaseSourceId < 0 || databaseTargetId < 0) {
+      Future.successful(localIds.map { (_, "") })
+    } else {
+      // Slick doesn't give facilities to create temporary table from `localIds` and
+      // make join against it. Until https://github.com/slick/slick/issues/799 is solved
+      val existingCrossMapsQuery = crossMaps.filter { cm =>
+        cm.dataSourceId === databaseSourceId && cm.localId.inSetBind(localIds)
+      }
+      val existingLocalIds = db.run(existingCrossMapsQuery.map { _.localId }.result)
+
+      val query = for {
+        sourceCrossMap <- existingCrossMapsQuery
+        mapping <- nameStringIndicies.filter { nsi =>
+          nsi.nameStringId === sourceCrossMap.nameStringId
+        }
+        targetCrossMap <- crossMaps.filter { cm =>
+          cm.dataSourceId === databaseTargetId && cm.nameStringId === mapping.nameStringId
+        }
+      } yield (sourceCrossMap.localId, targetCrossMap.localId)
+
+      for {
+        mapped <- db.run(query.result).map { _.distinct }
+        existing <- existingLocalIds
+      } yield mapped ++ (localIds diff existing).map { (_, "") }
     }
-    val existingLocalIds = db.run(existingCrossMapsQuery.map { _.localId }.result)
-
-    val query = for {
-      sourceCrossMap <- existingCrossMapsQuery
-      mapping <- nameStringIndicies.filter { nsi =>
-        nsi.nameStringId === sourceCrossMap.nameStringId
-      }
-      targetCrossMap <- crossMaps.filter { cm =>
-        cm.dataSourceId === databaseTargetId && cm.nameStringId === mapping.nameStringId
-      }
-    } yield (sourceCrossMap.localId, targetCrossMap.localId)
-
-    for {
-      mapped <- db.run(query.result).map { _.distinct }
-      existing <- existingLocalIds
-    } yield mapped ++ (localIds diff existing).map { (_, "") }
   }
 }
---
+++ resolver/src/test/scala/org/globalnames/resolver/CrossMapIntegrationSpec.scala
@@ -0,0 +1,43 @@
+package org.globalnames
+package resolver
+
+import model.NameStrings
+import slick.driver.PostgresDriver.api._
+
+class CrossMapIntegrationSpec extends SpecConfig {
+  val conn = Database.forConfig("postgresql-test")
+  val nameStrings = TableQuery[NameStrings]
+  val crossMap = new CrossMap(conn)
+
+  val dbSourceId = 8
+  val dbTargetId = 178
+
+  seed("test_resolver", "CrossMapIntegrationSpec")
+
+  describe("CrossMap") {
+    it("finds cross-map relations") {
+      whenReady(crossMap.execute(dbSourceId, dbTargetId, Seq("1", "1005101"))) { res =>
+        res.size shouldBe 2
+        res should contain(("1", ""))
+        res should contain(("1005101", "AF468436/#6"))
+      }
+    }
+
+    it("finds nothing when no local ids are provided") {
+      whenReady(crossMap.execute(dbSourceId, dbTargetId, Seq())) { _ shouldBe 'empty }
+    }
+
+    it("finds nothing on either DB ID is negative") {
+      whenReady(crossMap.execute(-1, dbTargetId, Seq())) { _ shouldBe Seq() }
+      whenReady(crossMap.execute(dbSourceId, -1, Seq())) { _ shouldBe Seq() }
+      whenReady(crossMap.execute(-1, dbTargetId, Seq("1"))) { _ shouldBe Seq(("1", "")) }
+      whenReady(crossMap.execute(dbSourceId, -1, Seq("1"))) { _ shouldBe Seq(("1", "")) }
+    }
+
+    it("finds nothing on non-existing DB ID") {
+      val absentId = 1000000
+      whenReady(crossMap.execute(dbSourceId, absentId, Seq("1"))) { _ shouldBe Seq(("1", "")) }
+      whenReady(crossMap.execute(absentId, dbTargetId, Seq("1"))) { _ shouldBe Seq(("1", "")) }
+    }
+  }
+}
