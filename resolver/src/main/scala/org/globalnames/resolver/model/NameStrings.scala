89b8efa11656df2d6feba7914831ebc95cd4bfb0
Alexander Myltsev <alexander@myltsev.com>
2016-10-19 19:18:47 +0300

    = Add support of None canonicals to NameStrings

--- resolver/src/main/scala/org/globalnames/resolver/model/NameStrings.scala
+++ resolver/src/main/scala/org/globalnames/resolver/model/NameStrings.scala
@@ -9,6 +9,8 @@ case class Name(id: UUID, value: String)
 case class NameString(name: Name, canonicalName: Option[Name])
 
 class NameStrings(tag: Tag) extends Table[NameString](tag, "name_strings") {
+  import NameStrings.emptyCanonicalUuid
+
   def id: Rep[UUID] = column[UUID]("id", O.PrimaryKey)
 
   def name: Rep[String] = column[String]("name")
@@ -19,8 +21,12 @@ class NameStrings(tag: Tag) extends Table[NameString](tag, "name_strings") {
 
   def * : ProvenShape[NameString] = ((id, name), (canonicalUuid, canonical)).shaped <> (
       { case (name, (canonicalNameUuid, canonicalNameValue)) =>
-        val canonicalName = for (uuid <- canonicalNameUuid; value <- canonicalNameValue)
-                            yield Name(uuid, value)
+        val canonicalName =
+          for {
+            uuid <- canonicalNameUuid
+            if uuid != emptyCanonicalUuid
+            value <- canonicalNameValue
+          } yield Name(uuid, value)
         NameString(Name.tupled.apply(name), canonicalName)
       },
       { ns: NameString =>
@@ -28,4 +34,9 @@ class NameStrings(tag: Tag) extends Table[NameString](tag, "name_strings") {
               (ns.canonicalName.map { _.id }, ns.canonicalName.map { _.value })))
       }
     )
+}
+
+object NameStrings {
+  private[NameStrings] val emptyCanonicalUuid =
+    UUID.fromString("a9456e61-bd30-53bc-8588-accb913cc64a")
 }
