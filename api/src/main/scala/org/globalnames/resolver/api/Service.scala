17256e69138514a98f871936f92f7f61d2f0ee95
Alexander Myltsev <alexander@myltsev.com>
2017-02-20 17:38:37 +0300

    = Handle bad JSON requests

--- api/src/main/scala/org/globalnames/resolver/api/Service.scala
+++ api/src/main/scala/org/globalnames/resolver/api/Service.scala
@@ -13,7 +13,7 @@ import QueryParser.SearchPart
 import model.Matches
 import Materializer.Parameters
 import slick.driver.PostgresDriver.api._
-import akka.http.scaladsl.model.StatusCodes
+import akka.http.scaladsl.model.StatusCodes._
 
 import scala.concurrent.{ExecutionContextExecutor, Future}
 import scalaz._
@@ -21,11 +21,11 @@ import Scalaz.{get => _, _}
 import spray.json._
 import akka.http.scaladsl.marshallers.sprayjson.SprayJsonSupport._
 import sangria.parser.{QueryParser => QueryParserSangria}
-import sangria.execution.Executor
+import sangria.execution.{ErrorWithResolver, Executor, QueryAnalysisError}
 import sangria.marshalling.sprayJson._
 
 trait Service extends NamestringsProtocols with CrossMapProtocols with NullOptions {
-  private val nameStringsMaxCount = 1000
+  import SchemaDefinition.nameStringsMaxCount
 
   implicit val system: ActorSystem
 
@@ -62,12 +62,20 @@ trait Service extends NamestringsProtocols with CrossMapProtocols with NullOptio
               case Some(obj: JsObject) => obj
               case _ => JsObject.empty
             }
+            val operation = fields.get("operationName") collect {
+              case JsString(op) => op
+            }
             QueryParserSangria.parse(query) match {
               case util.Success(queryAst) =>
                 complete(Executor.execute(SchemaDefinition.schema, queryAst,
-                         GnRepo(facetedSearcher), variables = vars))
+                         GnRepo(facetedSearcher), variables = vars, operationName = operation)
+                  .map(OK -> _)
+                  .recover {
+                    case error: QueryAnalysisError => BadRequest -> error.resolveError
+                    case error: ErrorWithResolver => InternalServerError -> error.resolveError
+                  })
               case util.Failure(error) =>
-                complete(StatusCodes.BadRequest, JsObject("error" -> JsString(error.getMessage)))
+                complete(BadRequest, JsObject("error" -> JsString(error.getMessage)))
             }
           }
         } ~ path("name_resolvers") {
