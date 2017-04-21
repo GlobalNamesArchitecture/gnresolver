3e066cc1c1c5796ffe36b14154589c3c2e4571b8
Alexander Myltsev <alexander@myltsev.com>
2017-04-21 21:49:55 +0300

    Appropriately rename schema objects

--- api/src/main/scala/org/globalnames/resolver/api/SchemaDefinition.scala
+++ api/src/main/scala/org/globalnames/resolver/api/SchemaDefinition.scala
@@ -20,21 +20,21 @@ trait SchemaDefinition extends DefaultJsonProtocol with CrossMapProtocols {
   implicit val nameRequestFormat: RootJsonFormat[NameRequest] =
     jsonFormat2(Resolver.NameRequest.apply)
 
-  val DataSource = ObjectType(
+  val DataSourceOT = ObjectType(
     "DataSource", fields[Unit, DataSource](
         Field("id", IntType, resolve = _.value.id)
       , Field("title", StringType, resolve = _.value.title)
     )
   )
 
-  val Name = ObjectType(
+  val NameOT = ObjectType(
     "Name", fields[Unit, Name](
         Field("id", IDType, resolve = _.value.id.toString)
       , Field("name", StringType, resolve = _.value.value)
     )
   )
 
-  val MatchType = ObjectType(
+  val MatchTypeOT = ObjectType(
     "MatchType", fields[Unit, model.MatchType](
         Field("value", StringType, resolve = _.value.toString)
       , Field("score", IntType, resolve = ctx => model.MatchType.score(ctx.value))
@@ -42,7 +42,7 @@ trait SchemaDefinition extends DefaultJsonProtocol with CrossMapProtocols {
     )
   )
 
-  val Classification = ObjectType(
+  val ClassificationOT = ObjectType(
     "Classification", fields[Unit, NameStringIndex](
         Field("path", OptionType(StringType), resolve = _.value.classificationPath)
       , Field("pathIds", OptionType(IDType), resolve = _.value.classificationPathIds)
@@ -50,7 +50,7 @@ trait SchemaDefinition extends DefaultJsonProtocol with CrossMapProtocols {
     )
   )
 
-  val Vernacular = ObjectType(
+  val VernacularOT = ObjectType(
     "Vernacular", fields[Unit, (VernacularString, VernacularStringIndex)](
         Field("id", IDType, resolve = _.value._1.id.toString)
       , Field("name", StringType, resolve = _.value._1.name)
@@ -58,7 +58,7 @@ trait SchemaDefinition extends DefaultJsonProtocol with CrossMapProtocols {
     )
   )
 
-  val AuthorScoreObj = ObjectType(
+  val AuthorScoreOT = ObjectType(
     "AuthorScore", fields[Unit, AuthorScore](
         Field("authorshipInput", StringType, resolve = _.value.authorshipInput)
       , Field("authorshipMatch", StringType, resolve = _.value.authorshipMatch)
@@ -66,50 +66,51 @@ trait SchemaDefinition extends DefaultJsonProtocol with CrossMapProtocols {
     )
   )
 
-  val ScoreObj = ObjectType(
+  val ScoreOT = ObjectType(
     "Score", fields[Unit, Score](
-        Field("matchType", MatchType, resolve = _.value.matchType)
+        Field("matchType", MatchTypeOT, resolve = _.value.matchType)
       , Field("nameType", OptionType(IntType), resolve = _.value.nameType)
-      , Field("authorScore", AuthorScoreObj, resolve = _.value.authorScore)
+      , Field("authorScore", AuthorScoreOT, resolve = _.value.authorScore)
       , Field("parsingQuality", IntType, resolve = _.value.parsingQuality)
     )
   )
 
-  val ResponseItem = ObjectType(
+  val ResponseItemOT = ObjectType(
     "ResponseItem", fields[GnRepo, model.MatchScored](
-        Field("name", Name, resolve = _.value.mtch.nameString.name)
-      , Field("canonicalName", OptionType(Name), resolve = _.value.mtch.nameString.canonicalName)
+        Field("name", NameOT, resolve = _.value.mtch.nameString.name)
+      , Field("canonicalName", OptionType(NameOT), resolve = _.value.mtch.nameString.canonicalName)
       , Field("surrogate", OptionType(BooleanType), resolve = _.value.mtch.nameString.surrogate)
       , Field("synonym", OptionType(BooleanType), resolve = _.value.mtch.synonym)
-      , Field("dataSource", OptionType(DataSource), resolve = _.value.mtch.dataSource)
+      , Field("dataSource", OptionType(DataSourceOT), resolve = _.value.mtch.dataSource)
       , Field("taxonId", IDType, resolve = _.value.mtch.nameStringIndex.taxonId)
       , Field("globalId", OptionType(IDType), resolve = _.value.mtch.nameStringIndex.globalId)
       , Field("localId", OptionType(IDType), resolve = _.value.mtch.nameStringIndex.localId)
-      , Field("classification", OptionType(Classification), resolve = _.value.mtch.nameStringIndex)
-      , Field("vernaculars", ListType(Vernacular), resolve = _.value.mtch.vernacularStrings)
-      , Field("matchType", MatchType, resolve = _.value.mtch.matchType)
-      , Field("preScore", ScoreObj, resolve = _.value.score)
+      , Field("classification", OptionType(ClassificationOT),
+              resolve = _.value.mtch.nameStringIndex)
+      , Field("vernaculars", ListType(VernacularOT), resolve = _.value.mtch.vernacularStrings)
+      , Field("matchType", MatchTypeOT, resolve = _.value.mtch.matchType)
+      , Field("preScore", ScoreOT, resolve = _.value.score)
     )
   )
 
-  val Response = ObjectType(
+  val ResponseOT = ObjectType(
     "Response", fields[GnRepo, model.Matches](
         Field("total", LongType, None, resolve = _.value.total)
       , Field("suppliedInput", OptionType(StringType), None, resolve = _.value.suppliedInput)
       , Field("suppliedId", OptionType(StringType), None, resolve = _.value.suppliedId)
-      , Field("results", ListType(ResponseItem), None, resolve = ctx => Scores.compute(ctx.value))
+      , Field("results", ListType(ResponseItemOT), None, resolve = ctx => Scores.compute(ctx.value))
     )
   )
 
   object CrossMap {
-    val Source = ObjectType(
+    val SourceOT = ObjectType(
       "Source", fields[Unit, CrossMapSearcher.Source](
           Field("dbId", IntType, resolve = _.value.dbId)
         , Field("localId", StringType, resolve = _.value.localId)
       )
     )
 
-    val Target = ObjectType(
+    val TargetOT = ObjectType(
       "Target", fields[Unit, CrossMapSearcher.Target](
           Field("dbSinkId", IntType, resolve = _.value.dbSinkId)
         , Field("dbTargetId", IntType, resolve = _.value.dbTargetId)
@@ -117,14 +118,14 @@ trait SchemaDefinition extends DefaultJsonProtocol with CrossMapProtocols {
       )
     )
 
-    val Result = ObjectType(
+    val ResultOT = ObjectType(
       "Result", fields[Unit, CrossMapSearcher.Result](
-          Field("source", Source, resolve = _.value.source)
-        , Field("target", ListType(Target), resolve = _.value.target)
+          Field("source", SourceOT, resolve = _.value.source)
+        , Field("target", ListType(TargetOT), resolve = _.value.target)
       )
     )
 
-    val Sinks = Argument("sinks", InputObjectType[CrossMapRequest](
+    val SinksArg = Argument("sinks", InputObjectType[CrossMapRequest](
       "CrossMapSink", List(
           InputField("dbSinkIds", ListInputType(IntType),
                      description = "Datasources to apply cross-map through")
@@ -132,58 +133,58 @@ trait SchemaDefinition extends DefaultJsonProtocol with CrossMapProtocols {
                      description = "Local IDs in `DBSourceId` to apply cross-map against")
       )
     ))
-    val DBSourceId = Argument("dataSourceId", IntType,
-                              description = "The database to cross-map data from")
-    val DBTargetId = Argument("dataTargetId", OptionInputType(IntType),
-                              description = "The database to cross-map data to")
+    val DBSourceIdArg = Argument("dataSourceId", IntType,
+                                 description = "The database to cross-map data from")
+    val DBTargetIdArg = Argument("dataTargetId", OptionInputType(IntType),
+                                 description = "The database to cross-map data to")
   }
 
-  val Id = Argument("id", StringType)
-  val Page = Argument("page", OptionInputType(IntType), 0)
-  val PerPage = Argument("perPage", OptionInputType(IntType), nameStringsMaxCount)
-  val WithSurrogates = Argument("withSurrogates", OptionInputType(BooleanType), false)
-  val WithVernaculars = Argument("withVernaculars", OptionInputType(BooleanType), false)
-  val NameRequestObj: InputObjectType[NameRequest] = InputObjectType[NameRequest]("name", List(
+  val IdArg = Argument("id", StringType)
+  val PageArg = Argument("page", OptionInputType(IntType), 0)
+  val PerPageArg = Argument("perPage", OptionInputType(IntType), nameStringsMaxCount)
+  val WithSurrogatesArg = Argument("withSurrogates", OptionInputType(BooleanType), false)
+  val WithVernacularsArg = Argument("withVernaculars", OptionInputType(BooleanType), false)
+  val NameRequestIOT: InputObjectType[NameRequest] = InputObjectType[NameRequest]("name", List(
       InputField("value", StringType)
     , InputField("suppliedId", OptionInputType(StringType))
   ))
-  val NamesRequest = Argument("names", ListInputType(NameRequestObj))
-  val DataSourceIds = Argument("dataSourceIds", OptionInputType(ListInputType(IntType)))
-  val SearchTerm = Argument("searchTerm", StringType)
+  val NamesRequestArg = Argument("names", ListInputType(NameRequestIOT))
+  val DataSourceIdsArg = Argument("dataSourceIds", OptionInputType(ListInputType(IntType)))
+  val SearchTermArg = Argument("searchTerm", StringType)
 
-  val QueryType = ObjectType(
+  val QueryTypeOT = ObjectType(
     "Query", fields[GnRepo, Unit](
-        Field("nameString", Response,
-          arguments = List(Id, Page, PerPage, WithSurrogates, WithVernaculars),
-          resolve = ctx => ctx.withArgs(Id, Page, PerPage, WithVernaculars) {
+        Field("nameString", ResponseOT,
+          arguments = List(IdArg, PageArg, PerPageArg, WithSurrogatesArg, WithVernacularsArg),
+          resolve = ctx => ctx.withArgs(IdArg, PageArg, PerPageArg, WithVernacularsArg) {
             (id, page, perPage, withVernaculars) =>
               ctx.ctx.nameStringByUuid(id, page, perPage, withVernaculars)
           }
         )
-      , Field("nameResolvers", ListType(Response),
-          arguments = List(NamesRequest, DataSourceIds, Page, PerPage, WithSurrogates,
-                           WithVernaculars),
-          resolve = ctx => ctx.withArgs(NamesRequest, DataSourceIds, Page, PerPage,
-                                        WithSurrogates, WithVernaculars) {
+      , Field("nameResolvers", ListType(ResponseOT),
+          arguments = List(NamesRequestArg, DataSourceIdsArg, PageArg, PerPageArg,
+                           WithSurrogatesArg, WithVernacularsArg),
+          resolve = ctx => ctx.withArgs(NamesRequestArg, DataSourceIdsArg, PageArg, PerPageArg,
+                                        WithSurrogatesArg, WithVernacularsArg) {
             (names, dataSourceIdsOpt, page, perPage, withSurrogates, withVernaculars) =>
               val dataSourceIds = dataSourceIdsOpt.map { _.toVector }.getOrElse(Vector())
               ctx.ctx.nameStrings(names.take(nameStringsMaxCount), dataSourceIds, page, perPage,
                                   withSurrogates, withVernaculars)
           }
         )
-      , Field("nameStrings", Response,
-          arguments = List(SearchTerm, Page, PerPage, WithSurrogates, WithVernaculars),
-          resolve = ctx => ctx.withArgs(SearchTerm, Page, PerPage, WithSurrogates,
-                                        WithVernaculars) { ctx.ctx.nameResolve })
-      , Field("crossMap", ListType(CrossMap.Result),
-          arguments = List(CrossMap.DBSourceId, CrossMap.DBTargetId, CrossMap.Sinks),
-          resolve = ctx => ctx.withArgs(CrossMap.DBSourceId, CrossMap.DBTargetId, CrossMap.Sinks) {
-            ctx.ctx.crossMapResolve
-          })
+      , Field("nameStrings", ResponseOT,
+          arguments = List(SearchTermArg, PageArg, PerPageArg,
+                           WithSurrogatesArg, WithVernacularsArg),
+          resolve = ctx => ctx.withArgs(SearchTermArg, PageArg, PerPageArg, WithSurrogatesArg,
+                                        WithVernacularsArg) { ctx.ctx.nameResolve })
+      , Field("crossMap", ListType(CrossMap.ResultOT),
+          arguments = List(CrossMap.DBSourceIdArg, CrossMap.DBTargetIdArg, CrossMap.SinksArg),
+          resolve = ctx => ctx.withArgs(CrossMap.DBSourceIdArg, CrossMap.DBTargetIdArg,
+                                        CrossMap.SinksArg) { ctx.ctx.crossMapResolve })
     )
   )
 
-  val schema = Schema(QueryType)
+  val schema = Schema(QueryTypeOT)
 
   case class GnRepo(facetedSearcher: FacetedSearcher, resolver: Resolver,
                     searcher: Searcher, crossMap: CrossMapSearcher)
